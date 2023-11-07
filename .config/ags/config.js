2ae89a329bf520ed08122641cade53094be07d5c
Sascha Loishandl <sascha.loishandl@gmail.com>
2023-11-07 09:33:09 +0100

    updated ags and hyprland

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,26 +1,47 @@
-import { Theme } from './theme/theme.js';
-import topbar from './layouts/topbar.js';
-import bottombar from './layouts/bottombar.js';
-import * as shared from './layouts/shared.js';
+'use strict';
 
-const layouts = {
-    topbar,
-    bottombar,
-};
+import TopBar from './js/bar/TopBar.js';
+import ScreenCorners from './js/screencorner/ScreenCorners.js';
+import Overview from './js/overview/Overview.js';
+import Dashboard from './js/dashboard/Dashboard.js';
+import OSD from './js/osd/OSD.js';
+import FloatingDock from './js/dock/FloatingDock.js';
+import Applauncher from './js/applauncher/Applauncher.js';
+import PowerMenu from './js/powermenu/PowerMenu.js';
+import Verification from './js/powermenu/Verification.js';
+import Desktop from './js/desktop/Desktop.js';
+import Notifications from './js/notifications/Notifications.js';
+import QuickSettings from './js/quicksettings/QuickSettings.js';
+import Lockscreen from './js/lockscreen/Lockscreen.js';
+import options from './js/options.js';
+import * as setup from './js/utils.js';
+import { forMonitors } from './js/utils.js';
 
-const monitors = ags.Service.Hyprland.HyprctlGet('monitors')
-    .map(mon => mon.id);
+setup.warnOnLowBattery();
+setup.scssWatcher();
+setup.globalServices();
+setup.activePlayer();
 
 export default {
+    maxStreamVolume: 1.05,
+    cacheNotificationActions: true,
     closeWindowDelay: {
-        'quicksettings': 300,
-        'dashboard': 300,
+        'quicksettings': options.windowAnimationDuration,
+        'dashboard': options.windowAnimationDuration,
     },
     windows: [
-        ...layouts[Theme.getSetting('layout')](monitors),
-        shared.ApplauncherPopup(),
-        shared.OverviewPopup(),
-        shared.PowermenuPopup(),
-        shared.VerificationPopup(),
-    ],
+        forMonitors(TopBar),
+        forMonitors(ScreenCorners),
+        forMonitors(OSD),
+        //forMonitors(FloatingDock),
+        forMonitors(Desktop),
+        forMonitors(Notifications),
+        forMonitors(Lockscreen),
+        //Applauncher(),
+        Overview(),
+        Dashboard(),
+        QuickSettings(),
+        PowerMenu(),
+        Verification(),
+    ].flat(2),
 };
