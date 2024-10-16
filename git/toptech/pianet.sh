b9f0929b595c07825b470d75b3a3173b71bc5a15
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-10-16 11:55:15 +0200

    added project tmux sh files

---
+++ git/toptech/pianet.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Project directories
+PROJECT_DIR_1="/home/sascha/git/toptech/PiANet/PiAPI/PiANetAPI"
+PROJECT_DIR_2="/home/sascha/git/toptech/PiANet/PiAstro/PiAstro"
+
+# Session name
+SESSION="pianet"
+
+# Check if the session already exists
+tmux has-session -t $SESSION 2>/dev/null
+
+if [ $? != 0 ]; then
+  # Session does not exist, create it
+  tmux new-session -d -s $SESSION -n "API"
+
+  # Window 1: First project
+  tmux send-keys -t $SESSION "cd $PROJECT_DIR_1" C-m
+  tmux split-window -h -t $SESSION
+  tmux send-keys -t $SESSION "cd $PROJECT_DIR_1" C-m
+  #tmux select-pane -t $SESSION.0
+  tmux split-window -v -t $SESSION.1
+  tmux send-keys -t $SESSION.2 "cd $PROJECT_DIR_1 && lazygit" C-m
+  tmux select-pane -t $SESSION.0
+
+  # Window 2: Second project
+  tmux new-window -t $SESSION -n "Astro"
+  tmux send-keys -t $SESSION:1 "cd $PROJECT_DIR_2" C-m
+  tmux split-window -h -t $SESSION:1
+  tmux send-keys -t $SESSION:1 "cd $PROJECT_DIR_2" C-m
+  #tmux select-pane -t $SESSION:1.0
+  tmux split-window -v -t $SESSION:1.1
+  tmux send-keys -t $SESSION:1.2 "cd $PROJECT_DIR_2 && lazygit" C-m
+  tmux select-pane -t $SESSION:1.0
+fi
+
+# Attach to the session
+tmux attach -t $SESSION
