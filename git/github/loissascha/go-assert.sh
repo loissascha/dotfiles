f401d2fca75af9e12303c4305a42f433857fa709
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-11-26 10:00:08 +0100

    assert dev

---
+++ git/github/loissascha/go-assert.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Project directories
+PROJECT_DIR_1="/home/sascha/git/github/loissascha/go-assert"
+
+cd $PROJECT_DIR_1
+
+# Session name
+SESSION="go-assert"
+
+# Check if the session already exists
+tmux has-session -t $SESSION 2>/dev/null
+
+if [ $? != 0 ]; then
+  cd $PROJECT_DIR_1
+  # Session does not exist, create it
+  tmux new-session -d -s $SESSION -n "code"
+
+  # Window 1: First project
+  # tmux send-keys -t $SESSION "cd $PROJECT_DIR_1" C-m
+  tmux split-window -h -t $SESSION
+  # tmux send-keys -t $SESSION "cd $PROJECT_DIR_1" C-m
+  #tmux select-pane -t $SESSION.0
+  tmux split-window -v -t $SESSION.1
+  # tmux send-keys -t $SESSION.2 "cd $PROJECT_DIR_1 && lazygit" C-m
+  tmux send-keys -t $SESSION.2 "lazygit" C-m
+  tmux select-pane -t $SESSION.0
+
+fi
+
+# Attach to the session
+tmux attach -t $SESSION
