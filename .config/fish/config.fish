979172b13691bf2dd6912e459a4fda2653bb7fb5
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-11-27 06:36:01 +0100

    Refactor: Update configuration files and aliases
    Refactors configuration files and updates Bun completions. Adds new aliases for tmuxinator, development, and git. Updates path configurations for various tools including Go, .NET, and opencode. Modifies fish color settings and enhances prompt appearance with Pure and Starship. Includes optimizations for disk usage analysis and package management.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,60 +1,52 @@
-source ~/.config/fish/themes/kanagawa.fish
-
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-
-    alias ga="git add"
+	# Commands to run in interactive sessions can go here
+	alias tnew="tmuxinator new"
+	alias tstart="tmuxinator start"
+	alias tstop="tmuxinator stop"
+	alias tlist="tmuxinator list"
+	alias dev="distrobox enter development"
+	alias vim="nvim"
+	alias rec="asciinema"
+	alias croc="croc"
+	alias nvimgodot="nvim --listen ./godothost"
+	alias ga="git add"
+	alias gs="git status"
+	alias gp="git push"
     alias gc="git commit -m"
-    alias gp="git push"
     alias gb="git checkout -b"
+	alias aic="aicommit --confirm"
 	alias btrfsbalance="sudo btrfs balance start -dusage=50 /"
 	alias analyserootdisk="ncdu / --exclude /home"
 	alias analysehomedisk="ncdu /home"
 	alias cleanyaycache="yay -Scc"
     alias gitui="lazygit"
     alias multi="tmux"
-    alias du="dust"
-    alias runGodotMono="cd ~/Software/Godot_v4.2.2-stable_mono && nohup ./Godot_v4.2.2-stable_mono_linux.x86_64 & disown"
-    alias vim="nvim"
-    alias lvim="/home/sascha/.local/bin/lvim"
-    # alias cat="bat"
     alias benchmark="hyperfine"
-    alias pianetdev="/home/sascha/git/toptech/pianetapi.sh"
-    alias piaastrodev="/home/sascha/git/toptech/pianetastro.sh"
-    alias piagodev="/home/sascha/git/toptech/pianetgo.sh"
-    alias mangoverlaydev="/home/sascha/git/github/loissascha/mangoverlay.sh"
-	alias dbplanningdev="/home/sascha/git/github/loissascha/db-planning-tool.sh"
-	alias expochoresdev="/home/sascha/git/github/loissascha/expo-chores.sh"
-    alias benchmarkdev="/home/sascha/git/github/loissascha/benchmark.sh"
-    alias mysqlmanagerdev="/home/sascha/git/github/loissascha/mysqlmanager.sh"
-	alias appmemorydev="/home/sascha/git/github/loissascha/go-appmemory.sh"
-	alias assertdev="/home/sascha/git/github/loissascha/go-assert.sh"
-	alias loggerdev="/home/sascha/git/github/loissascha/go-logger.sh"
 	alias countlines="tokei"
     alias mc="yazi"
 	alias design="nwg-look"
-	alias tnew="tmuxinator new"
-	alias tstart="tmuxinator start"
-	alias tstop="tmuxinator stop"
-	alias tlist="tmuxinator list"
-    #alias fm="yazi"
-    
-    #export PiANetConnection='server=localhost;port=30306;database=piaNetTest;user=root;password=root'
-    export PiANetConnection='Host=localhost;Port=5432;Database=piaNetTest;Username=postgres;Password=root'
-    export PATH=$PATH:$HOME/.local/bin
-    export PATH=$PATH:/usr/bin
-    export PATH=$PATH:$HOME/.dotnet/tools:$HOME/go/bin:$HOME/.local/bin
-	export EDITOR=nvim
 
-	export ANDROID_HOME=$HOME/Android/Sdk
-	export ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle
+	fish_add_path $HOME/.local/bin # correct way to add stuff to your $PATH in fish
+	fish_add_path $HOME/go/bin
+	fish_add_path $HOME/.dotnet/tools
+	export EDITOR=vim
+	export PiANetConnection='Host=192.168.0.96;Port=5432;Database=piaNetTest;Username=postgres;Password=root'
 
-    #fastfetch
-    gofetch
 end
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
+set -gx GEMINI_API_KEY (cat ~/.config/gemini_token)
+
+if command -v starship >/dev/null 2>&1
+    starship init fish | source
+end
+
+zoxide init --cmd cd fish | source
+fastfetch
 starship init fish | source
+
+# opencode
+fish_add_path /home/sascha/.opencode/bin
